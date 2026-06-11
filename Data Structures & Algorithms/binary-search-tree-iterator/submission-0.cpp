1e98e9fcea9ec5a608024830a022a73c7c84049c
Sharva05 <137437954+Sharva05@users.noreply.github.com>
2026-06-11 18:22:09 +0530

    Add: binary-search-tree-iterator - submission-0

---
+++ Data Structures & Algorithms/binary-search-tree-iterator/submission-0.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BSTIterator {
+    stack<TreeNode* >s;
+    void pushAllLeft(TreeNode* node) {
+        while(node){
+            s.push(node);
+            node=node->left;
+        }
+    }
+public:
+    BSTIterator(TreeNode* root) {
+        pushAllLeft(root);
+    }
+    
+    int next() {
+        TreeNode* next=s.top();
+        s.pop();
+        pushAllLeft(next->right);
+        return next->val;
+    }
+    
+    bool hasNext() {
+        return !s.empty();
+    }
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
